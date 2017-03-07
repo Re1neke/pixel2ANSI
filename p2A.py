c7df350ebac83a5002902f7b2f90e9c32cd5b6fc
Max <reinekekun@gmail.com>
2017-03-07 12:35:26 +0200

    First code :)
    Was added first and very dirty code-sheet and little swiss flag image
    for testing script.

---
+++ p2A.py
@@ -0,0 +1,132 @@
+from PIL import Image
+import sys
+
+#image_name = sys.argv[1]    #get script's paramets 
+#script_name = sys.argv[2]
+image_name = 'swiss.png' #TODO: delete
+script_name = 'test.sh'
+        
+        
+class pixel2ANSI:
+    def __init__(self, image_name):
+        print("********************")
+        self.path = image_name
+        self.create_obj(self.path)
+        self.create_list(self.img_obj)
+        self.fill_list()
+        print("********************")
+
+    def create_obj(self, path):
+        image = Image.open(path)
+        size = image.size
+        self.img_obj = image.load()
+        self.width = size[0]
+        self.height = size[1]
+        print((
+            "#Image object created.\n\t-Size of picture is: "
+            +str(self.width)
+            +"x"+str(self.height)+"px"
+            ))
+
+    def create_list(self, obj):
+        if self.height%2==1:
+            list_len = self.height//2 + 1
+        else:
+            list_len = self.height//2
+        self.image_list = [[[0] * 2 for e in range(self.width)] for i in range(list_len)]
+        print((
+            "#Array created.\n\t-Width: "
+            +str(len(self.image_list[0]))
+            +"\n\t-Height:"
+            +str(len(self.image_list))
+            ))
+
+    def fill_list(self):
+        height = 0  #height of image
+        l_height = 0#height of result list
+        while height < self.height:
+            width = 0
+            if height%2==0  or height == 0:
+                while width < self.width:
+                    color1 = self.img_obj[width, height]
+                    self.image_list[l_height][width][0] = color1
+                    width += 1
+                height += 1
+            else:
+                while width < self.width:
+                    color2 = self.img_obj[width, height]
+                    self.image_list[l_height][width][1] = color2
+                    width += 1
+                height += 1
+                l_height += 1
+        print("#Array filled.")
+
+lol = pixel2ANSI(image_name)
+
+esc="\033"
+rst = esc+"[0;00m"
+
+fgr="[38;5;" #for foreground color
+bgr="[48;5;" #for background color
+
+def bg_d(color_f):
+    def wraper():
+        color = esc+bgr+str(color_f)+"m"
+        return(color)
+    return wraper()
+
+def fg_d(color_f):
+    def wraper():
+        color = esc+fgr+str(color_f)+"m"
+        return(color)
+    return wraper()
+
+#@color_decorator
+def trans_col(rgb):
+    colors = [
+        [(0,0,0), 0],       #Black
+        [(128,0,0), 1],     #Maroon
+        [(0,128,0), 2],     #Green
+        [(128,128,0), 3],   #Olive
+        [(0,0,128), 4],     #Navy
+        [(128,0,128), 5],   #Purple
+        [(0,128,128), 6],   #Teal
+        [(192,192,192), 7], #Silver
+        [(128,128,128), 8], #Grey
+        [(255,0,0), 9],     #Red
+        [(0,255,0), 10],    #Lime
+        [(255,255,0), 11],  #Yellow
+        [(0,0,255), 12],    #Blue
+        [(255,0,255), 13],  #Fuchia
+        [(0,255,255), 14],  #Aqua
+        [(255,255,255), 15],#White
+    ]
+    if rgb == 0:
+        code = " "
+    for color in colors:
+        #return str(color[1])print(str(rgb)+" "+str(color[0]))
+        if color[0]==rgb:
+            code = color[1]
+            break
+        else:
+            code = colors[2][1]
+            
+    return str(code)
+
+script = open(script_name, "w", encoding='utf-8')
+
+for row in lol.image_list:
+    row_r = ""
+    for symb in row:
+        if symb[0]==symb[1]:
+            #print("___"+trans_col(symb[0]))
+            row_r += fg_d(trans_col(symb[0])) + "█" + rst
+        else:
+            #print(trans_col(symb[0]))
+            #print(trans_col(symb[1]))
+            row_r += bg_d(trans_col(symb[0])) + fg_d(trans_col(symb[1])) + "▄" + rst
+    script.write(row_r+"\n")
+
+script.close()
+
+print("-Writen to file")
